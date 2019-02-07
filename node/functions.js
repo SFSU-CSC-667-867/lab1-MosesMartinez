45a692ab147e82adb2e056595e18da8d085fc879
Moses Martinez <mm.mosesmartinez@gmail.com>
2019-02-06 20:25:43 -0800

    Function update

--- node/functions.js
+++ node/functions.js
@@ -15,5 +15,11 @@ arrow('hi')
 // for practice
 
 // make an arrow function with no block body
-
+  const myArrow = (y) => y + 1;
+  
+  console.log(myArrow(9));
 // make a function that returns a function
+
+const functionFactory = () => () => console.log('This is wierd'); 
+const a = functionFactory();
+a();
